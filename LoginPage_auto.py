02465ecb4a8545719dc08712d65dd54389fe094a
Amith Kulkarni <103172775+amithuu@users.noreply.github.com>
2023-05-20 08:42:23 +0530

    fix: login -page constructor to variable

--- LoginPage_auto.py
+++ LoginPage_auto.py
@@ -8,21 +8,21 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 class LoginPage:
 
-    def __init__(self):
-        self.driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+
     def loginpage(self):
-        self.driver.get("https://test-talentplace.vercel.app/")
+        driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+        driver.get("https://test-talentplace.vercel.app/")
 
         # Pop-up of WorkShop CLose 'X' mark.
         # WebDriverWait(driver, 20).until(ec.element_to_be_clickable((By.XPATH,"//button[@aria-label='Close']"))).click()
 
         # Login Button
-        WebDriverWait(self.driver, 20).until(ec.element_to_be_clickable((By.XPATH, "//button[normalize-space()='Login']"))).click()
+        WebDriverWait(driver, 20).until(ec.element_to_be_clickable((By.XPATH, "//button[normalize-space()='Login']"))).click()
 
         # Login Page
-        WebDriverWait(self.driver, 20).until(ec.element_to_be_clickable((By.NAME, "email"))).send_keys("amithkulkarni98@gmail.com")
-        WebDriverWait(self.driver, 20).until(ec.element_to_be_clickable((By.NAME, "password"))).send_keys("New@1234")
-        WebDriverWait(self.driver, 20).until(ec.element_to_be_clickable((By.XPATH, "//button[normalize-space()='Login']"))).click()
+        WebDriverWait(driver, 20).until(ec.element_to_be_clickable((By.NAME, "email"))).send_keys("amithkulkarni98@gmail.com")
+        WebDriverWait(driver, 20).until(ec.element_to_be_clickable((By.NAME, "password"))).send_keys("New@1234")
+        WebDriverWait(driver, 20).until(ec.element_to_be_clickable((By.XPATH, "//button[normalize-space()='Login']"))).click()
         time.sleep(3)
 
 
