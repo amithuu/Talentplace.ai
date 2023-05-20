2f7bbdd7b3eeb0cb9fdae59a58683d27389809cb
Amith Kulkarni <103172775+amithuu@users.noreply.github.com>
2023-05-20 09:24:11 +0530

    fix: login -page constructorN to variable

--- rihbot.py
+++ rihbot.py
@@ -9,26 +9,26 @@ from webdriver_manager.chrome import ChromeDriverManager
 class LoginPage:
 
     def __init__(self):
-        self.driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+        driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
     def loginpage(self):
-        self.driver.get("https://rigbot.com/")
+        driver.get("https://rigbot.com/")
 
         # # Pop-up of WorkShop CLose 'X' mark.
         # # WebDriverWait(driver, 20).until(ec.element_to_be_clickable((By.XPATH,"//button[@aria-label='Close']"))).click()
-        # main = self.driver.current_window_handle
+        # main = driver.current_window_handle
         # Login Button
-        # WebDriverWait(self.driver, 20).until(ec.element_to_be_clickable((By.XPATH, "//button[normalize-space()='Login']"))).click()
-        log =self.driver.execute_script("return document.getElementsByTagName('a')[11];")
-        self.driver.execute_script("arguments[0].click();", log)
+        # WebDriverWait(driver, 20).until(ec.element_to_be_clickable((By.XPATH, "//button[normalize-space()='Login']"))).click()
+        log =driver.execute_script("return document.getElementsByTagName('a')[11];")
+        driver.execute_script("arguments[0].click();", log)
         time.sleep(10)
 
-        # wind = self.driver.current_window_handle
-        # if self.driver.current_window_handle != main:
-        #     self.driver.switch_to.window(wind)
+        # wind = driver.current_window_handle
+        # if driver.current_window_handle != main:
+        #     driver.switch_to.window(wind)
         # Login Page
-        self.driver.find_element(By.NAME, 'username').send_keys("amith")
-        self.driver.find_element(By.NAME, 'password').send_keys("kulkarni")
-        self.driver.find_element(By.XPATH, "//button[text()='Log In']").click()
+        driver.find_element(By.NAME, 'username').send_keys("amith")
+        driver.find_element(By.NAME, 'password').send_keys("kulkarni")
+        driver.find_element(By.XPATH, "//button[text()='Log In']").click()
         time.sleep(3)
 
 
