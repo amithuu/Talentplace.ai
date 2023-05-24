a0f782c9fbef2ac7c4c48173224a280151373f3b
amithuu <amithkulkarni98@gmail.com>
2023-05-24 14:47:55 +0530

    `perf'– performance improvements

--- Test.py
+++ Test.py
@@ -9,7 +9,7 @@ from selenium.webdriver import ActionChains, Keys
 
 driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
 driver.maximize_window()
-k = 25
+k = 32
 name = "Autotest"
 email = f"autotest{k}@g.co"
 password = "New@1234"
@@ -345,7 +345,7 @@ class Automation:
         save_and_next.click()
         time.sleep(2)
 
-    def profile_complition(self):
+    def profile_completion(self):
         # Next
         next_ = WebDriverWait(driver, 20).until(ec.element_to_be_clickable((By.XPATH, "//*[@class='css-hboir5']/button[2]")))
         next_.click()
@@ -529,7 +529,6 @@ class Automation:
         # Description
         description = WebDriverWait(driver, 20).until(ec.element_to_be_clickable((By.XPATH, "//*[@role='region']/form/div/div[4]//p")))
         description.send_keys("this is my first Publication")
-        time.sleep(2)
         # Save
         save = WebDriverWait(driver, 20).until(ec.element_to_be_clickable((By.XPATH, "//button[text()='Save']")))
         save.click()
@@ -626,8 +625,6 @@ class Automation:
         time.sleep(3)
 
     def causes(self):
-        driver.get("https://test-talentplace.vercel.app/onboarding/causes")
-        time.sleep(2)
         """Causes"""
         # Select The Causes Max[6]
         for i in range(1, 7):
@@ -699,28 +696,152 @@ class Automation:
     def dashboard(self):
         dashboard = WebDriverWait(driver, 20).until(ec.element_to_be_clickable((By.XPATH, "//button[text()='Dashboard']")))
         dashboard.click()
+        time.sleep(3)
+
+    def take_assesment(self):
+        # If user already Logged In.
+        WebDriverWait(driver, 20).until(ec.element_to_be_clickable((By.XPATH, "//*[text()='Take Assessment']"))).click()
+        time.sleep(2)
+        # Click on check Box
+        time.sleep(3)
+        check_box = WebDriverWait(driver, 20).until(ec.element_to_be_clickable((By.XPATH, "//*[@id='root']/div/div[2]/div[1]/div[2]/div[1]//span")))
+        check_box.click()
+        # Start Test
+        start_test = WebDriverWait(driver, 20).until(ec.element_to_be_clickable((By.XPATH, "//*[@id='root']/div/div[2]/div[1]/div[2]/div[1]/div/div[3]//button")))
+        start_test.click()
+        # Answers
+        for i in range(1, 10):
+            for j in range(1, 6):
+                answer = WebDriverWait(driver, 20).until(ec.element_to_be_clickable((By.XPATH, f"//*[@id='root']/div/div[2]/div[1]/div[2]/div[2]//p[{j}]")))
+                answer.click()
+        # Click on submit
+        submit = WebDriverWait(driver, 20).until(ec.element_to_be_clickable((By.XPATH, "//*[@id='root']/div/div[2]/div[1]/div[3]//button[2]")))
+        submit.click()
+        time.sleep(15)
+        # Click on view Report
+        view_report = WebDriverWait(driver, 20).until(ec.element_to_be_clickable((By.XPATH, "//*[@id='root']/div/div[2]//button")))
+        view_report.click()
+        time.sleep(5)
+
+    def personality_assesment(self):
+        """Personality Assessment"""
+        # Overview
+        # page_down.send_keys(Keys.PAGE_DOWN)
+        # page_down.send_keys(Keys.PAGE_DOWN)
+        # time.sleep(3)
+        # page_down.send_keys(Keys.PAGE_UP)
+        # page_down.send_keys(Keys.PAGE_UP)
+        time.sleep(2)
+        # Click on Carrier Strength
+        carrier_strength = driver.execute_script("return document.getElementsByTagName('a')[3];")
+        driver.execute_script("arguments[0].click();", carrier_strength)
+        driver.find_element(By.TAG_NAME, value="body").send_keys(Keys.PAGE_DOWN)
+        driver.find_element(By.TAG_NAME, value="body").send_keys(Keys.PAGE_DOWN)
+        time.sleep(2)
+        driver.find_element(By.TAG_NAME, value="body").send_keys(Keys.PAGE_UP)
+        driver.find_element(By.TAG_NAME, value="body").send_keys(Keys.PAGE_UP)
+        time.sleep(2)
+        for i in range(1, 7):
+            tablist = WebDriverWait(driver, 20).until(ec.element_to_be_clickable((By.XPATH, f"//*[@role='tablist']//button[{i}]")))
+            tablist.click()
+            driver.find_element(By.TAG_NAME, value="body").send_keys(Keys.PAGE_DOWN)
+            driver.find_element(By.TAG_NAME, value="body").send_keys(Keys.PAGE_DOWN)
+            time.sleep(2)
+            driver.find_element(By.TAG_NAME, value="body").send_keys(Keys.PAGE_UP)
+            driver.find_element(By.TAG_NAME, value="body").send_keys(Keys.PAGE_UP)
+            time.sleep(2)
+        # Click on Upgrade Button[//*[@id='root']/div[2]/div[2]/div/div/div/div/div/div[2]/div[{6}]/div/div[{3}]
+        # //button]{1,6} for tabs and {3,4} for inside tabs change
+        # Next and Previous
+        # next_ = WebDriverWait(driver, 20).until(ec.element_to_be_clickable((By.XPATH, "//*[@id='root']/div[2]/div[2]/div/div/div[3]/a[2]//button")))
+        # next_.click()
+        # back_ = WebDriverWait(driver, 20).until(ec.element_to_be_clickable((By.XPATH, "//*[@id='root']/div[2]/div[2]/div/div/div[3]/a[1]//button")))
+        # back_.click()
+        upgrade = WebDriverWait(driver, 20).until(ec.element_to_be_clickable((By.XPATH, "//*[@id='root']/div[2]/div[2]/div/div/div/div/div/div[2]/div[6]/div/div[3]//button")))
+        upgrade.click()
+        time.sleep(2)
+        driver.back()
+        time.sleep(2)
+        # Click on Carrier Options
+        carrier_options = driver.execute_script("return document.getElementsByTagName('a')[4];")
+        driver.execute_script("arguments[0].click();", carrier_options)
+        driver.find_element(By.TAG_NAME, value="body").send_keys(Keys.PAGE_DOWN)
+        driver.find_element(By.TAG_NAME, value="body").send_keys(Keys.PAGE_DOWN)
+        driver.find_element(By.TAG_NAME, value="body").send_keys(Keys.PAGE_DOWN)
+        time.sleep(2)
+        driver.find_element(By.TAG_NAME, value="body").send_keys(Keys.PAGE_UP)
+        driver.find_element(By.TAG_NAME, value="body").send_keys(Keys.PAGE_UP)
+        driver.find_element(By.TAG_NAME, value="body").send_keys(Keys.PAGE_UP)
+        time.sleep(2)
+        # CLick on Resume Improvements
+        resume_improvements = driver.execute_script("return document.getElementsByTagName('a')[5];")
+        driver.execute_script("arguments[0].click();", resume_improvements)
+        driver.find_element(By.TAG_NAME, value="body").send_keys(Keys.PAGE_DOWN)
+        driver.find_element(By.TAG_NAME, value="body").send_keys(Keys.PAGE_DOWN)
+        time.sleep(2)
+        driver.find_element(By.TAG_NAME, value="body").send_keys(Keys.PAGE_UP)
+        driver.find_element(By.TAG_NAME, value="body").send_keys(Keys.PAGE_UP)
+        time.sleep(2)
+        # CLick on Interview Questions
+        interview_questions = driver.execute_script("return document.getElementsByTagName('a')[6];")
+        driver.execute_script("arguments[0].click();", interview_questions)
+        driver.find_element(By.TAG_NAME, value="body").send_keys(Keys.PAGE_DOWN)
+        driver.find_element(By.TAG_NAME, value="body").send_keys(Keys.PAGE_DOWN)
+        time.sleep(2)
+        driver.find_element(By.TAG_NAME, value="body").send_keys(Keys.PAGE_UP)
+        driver.find_element(By.TAG_NAME, value="body").send_keys(Keys.PAGE_UP)
+        time.sleep(2)
+        # CLick on Leadership Potential
+        leadership_potential = driver.execute_script("return document.getElementsByTagName('a')[7];")
+        driver.execute_script("arguments[0].click();", leadership_potential)
+        driver.find_element(By.TAG_NAME, value="body").send_keys(Keys.PAGE_DOWN)
+        driver.find_element(By.TAG_NAME, value="body").send_keys(Keys.PAGE_DOWN)
+        time.sleep(2)
+        driver.find_element(By.TAG_NAME, value="body").send_keys(Keys.PAGE_UP)
+        driver.find_element(By.TAG_NAME, value="body").send_keys(Keys.PAGE_UP)
+        time.sleep(2)
+        # CLick on Self Development
+        self_development = driver.execute_script("return document.getElementsByTagName('a')[8];")
+        driver.execute_script("arguments[0].click();", self_development)
+        driver.find_element(By.TAG_NAME, value="body").send_keys(Keys.PAGE_DOWN)
+        driver.find_element(By.TAG_NAME, value="body").send_keys(Keys.PAGE_DOWN)
+        time.sleep(2)
+        driver.find_element(By.TAG_NAME, value="body").send_keys(Keys.PAGE_UP)
+        driver.find_element(By.TAG_NAME, value="body").send_keys(Keys.PAGE_UP)
+        time.sleep(2)
+        # click on Overview
+        overview = driver.execute_script("return document.getElementsByTagName('a')[2];")
+        driver.execute_script("arguments[0].click();", overview)
+        driver.find_element(By.TAG_NAME, value="body").send_keys(Keys.PAGE_DOWN)
+        driver.find_element(By.TAG_NAME, value="body").send_keys(Keys.PAGE_DOWN)
+        time.sleep(2)
+        driver.find_element(By.TAG_NAME, value="body").send_keys(Keys.PAGE_UP)
+        driver.find_element(By.TAG_NAME, value="body").send_keys(Keys.PAGE_UP)
+        time.sleep(2)
 
 
 ref = Automation()
-# ref.signup()
+ref.signup()
 ref.login()
-# ref.welcome_page()
-# ref.personal_details()
-# ref.nextback()
-# ref.experience()
-# ref.education()
-# ref.cognitive_skills()
-# ref.carrier_summary()
-# ref.customize_profile()
-# ref.profile_complition()
-# ref.certificate()
-# ref.publication()
-# ref.patent()
-# ref.portfolio()
-# ref.voluntary_roles()
-# ref.honor_rewards()
+ref.welcome_page()
+ref.personal_details()
+ref.nextback()
+ref.experience()
+ref.education()
+ref.cognitive_skills()
+ref.carrier_summary()
+ref.customize_profile()
+ref.profile_completion()
+ref.certificate()
+ref.publication()
+ref.patent()
+ref.portfolio()
+ref.voluntary_roles()
+ref.honor_rewards()
 ref.causes()
 ref.hobbies()
 ref.languages()
 ref.dashboard()
+ref.take_assesment()
+ref.personality_assesment()
 
